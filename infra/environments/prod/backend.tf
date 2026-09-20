df1d051be36cdbba18b30cfa73f3e2e2d216e86f
VicenteZambrano12 <vicente.zambrano.andrada@gmail.com>
2026-09-20 18:47:50 +0200

    feat: terraform deployed

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -1,7 +1,6 @@
-# NOTE: This GCS bucket was provisioned manually via gcloud (not managed by Terraform).
 terraform {
   backend "gcs" {
-    bucket = "terraform-bucket-portfolio"
+    bucket = "terraform_bucket_portfolio"
     prefix = "basic_rag_app_tfstate"
   }
 }
